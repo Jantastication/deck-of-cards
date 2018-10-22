6bb4c8f7a004bf8cb84cdf1c2f0416e9803a3f55
github username <github email address>
2018-10-22 12:03:52 -0500

    done

---
+++ run.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+require_relative "./solution.rb"
+
+card = Card.new("Hearts", "Q")
+card1 = Card.new("Spades", 2)
+
+deck = Deck.new
+deck1 = Deck.new
+
+binding.pry
+0
--- solution.rb
+++ solution.rb
@@ -1,7 +1,45 @@
 class Deck
 
+    attr_accessor :cards
+
+    @cards = []
+
+    def initialize
+        @cards =[]
+        ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        
+        suits.each do |suit|
+            ranks.each do | rank |
+                @cards << Card.new(suit, rank)
+            end
+        end
+    
+       
+    end
+
+    def card
+        @card.first
+    end
+    
+    def choose_card
+        @cards.delete_at(rand(@cards.length))
+    end
+
 end
 
 class Card
 
+     attr_reader :rank, :suit
+
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit 
+    end
+
 end
+
+
+#Construct a class for a Deck of cards, which has an attribute cards consisting of an array of 52 Card objects.
+#A Card consists of a rank ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K") paired with a suit ("Hearts", "Clubs", "Diamonds", "Spades")
+#Your Deck should have a method #choose_card which selects a random card from the deck and removes it.
